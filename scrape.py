30246921b54a175cc7b338ee1ec372d8c4035c6b
adamnew123456 <adamnew123456@gmail.com>
2016-11-10 20:02:03 -0500

    Add support for scraping more bill information
    - The name of the bill
    - The publihsed date of the bill
    - When the bill was requested and by what committee
    - What numbered Congress the made the request

--- scrape.py
+++ scrape.py
@@ -2,13 +2,17 @@
 Scrapes the given CBO page, providing all the dollar figures found in the page
 and the context around them.
 """
+import datetime
+import dateutil.parser
 import re
 import sys
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
-REGEX = re.compile(r'(\w+\W){0,10}(\$[0-9,]+(\.[0-9]+)?)\W(billion|million|thousand)+\Wover\Wthe\W[0-9]+-[0-9]+\Wperiod')
+COST_CONTEXT = re.compile(r'(\w+\W){0,10}(\$[0-9,]+(\.[0-9]+)?)\W(billion|million|thousand)+\Wover\Wthe\W[0-9]+-[0-9]+\Wperiod')
+LONG_DATE = re.compile('(January|February|March|April|May|June|July|August|September|October|November|December) [0-9]{1,2}, [0-9]{4}')
+COMMITTEE_NAME = re.compile('by the (.+) on (January|February|March|April|May|June|July|August|September|October|November|December)')
 
 def split_sentences(text):
     """
@@ -17,27 +21,85 @@ def split_sentences(text):
     sentences = re.split('[.!?]+', text)
     return sentences
 
-def get_dollar_contexts(url):
+def compute_congress(bill_date):
     """
-    Retrieves the given CBO page, and returns a list of sentences with dollar
-    figures in them.
+    Figures out what Congress applies to the given date.
     """
-    with urlopen(url) as page:
-        soup = BeautifulSoup(page.read(), 'html.parser')
+    # Note that each Congress seats on January 3rd, so consider an early
+    # January date as the previous Congress
+    if bill_date.month == 1 and bill_date.day < 3:
+        bill_date = datetime.date(bill_date.year - 1, 12, 31)
 
-    contexts = []
-    article_pars = soup.article.find_all('p')
+    return (bill_date.year - 1789) // 2 + 1
 
-    for paragraph in article_pars:
-        paragraph_text = paragraph.get_text()
-        for match in REGEX.finditer(paragraph_text):
-            print(match.group())
+class BillInfo:
+    """
+    A single CBO page on a particular bill.
+    """
+    def __init__(self, url):
+        self.url = url
+        self.name = None
+        self.cost_suggestions = []
+        self.pub_date = None
+        self.request_date = None
+        self.committee = None
+        self.congress = None
 
-    return contexts 
+        with urlopen(url) as page:
+            self.soup = BeautifulSoup(page.read(), 'html.parser')
+
+        self._process()
+
+    def _process(self):
+        self.name = self.soup.h1.get_text()
+
+        self.cost_suggestions = self._get_dollar_contexts()
+        pub_date_raw = self.soup.find(
+            'span',
+            {'class': 'date-display-single', 'property': 'dc:date'}
+        )['content']
+
+        self.pub_date = dateutil.parser.parse(pub_date_raw).date()
+
+        request_committee_top = self.soup.find('div', {'class': 'summary'})
+        request_committee_text = request_committee_top.get_text()
+
+        self.committee = COMMITTEE_NAME.search(request_committee_text).group(1)
+        request_date_raw = LONG_DATE.search(request_committee_text).group()
+        self.request_date = dateutil.parser.parse(request_date_raw).date()
+
+        self.congress = compute_congress(self.request_date)
+
+    def _get_dollar_contexts(self):
+        """
+        Returns a list of "word contexts" that surround likely cost contexts.
+        """
+        contexts = []
+        article_pars = self.soup.article.find_all('p')
+
+        for paragraph in article_pars:
+            paragraph_text = paragraph.get_text()
+            for match in COST_CONTEXT.finditer(paragraph_text):
+                contexts.append(match.group())
+
+        return contexts
 
 if len(sys.argv) == 1:
     print(sys.argv[0], '<url>')
     sys.exit(1)
 
-for sentence in get_dollar_contexts(sys.argv[1]):
-    print(sentence)
+info = BillInfo(sys.argv[1])
+print('#', info.name)
+
+print('## Requested')
+print('On {} by the {} of the {} Congress'.format(
+            info.request_date,
+            info.committee, 
+            info.congress))
+
+print('## Published')
+print(info.pub_date)
+
+print('## Cost Suggestions')
+for sentence in info.cost_suggestions:
+    print(' -', sentence)
